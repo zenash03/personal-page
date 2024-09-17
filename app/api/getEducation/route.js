587415af62dee3390f4ee436d392aa659f6fbea9
wyrtzen <wyrtzen.ecc@gmail.com>
2024-09-18 00:10:36 +0700

    Fix CROS

--- app/api/getEducation/route.js
+++ app/api/getEducation/route.js
@@ -9,22 +9,11 @@ export async function GET(request) {
         await connect();
 
         const data = await EducationModel.find().populate('relatedExperience');
-
-        // Create a NextResponse with CORS headers
         const response = new NextResponse(JSON.stringify({ data }), { status: 200 });
-        response.headers.set('Access-Control-Allow-Origin', '*'); // Allow all origins or specify the allowed origin
-        response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-        response.headers.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
-
         return response;
     }
     catch (error) {
-        // Create a NextResponse with CORS headers for error responses
         const response = new NextResponse(JSON.stringify({ error: error.message }), { status: 500 });
-        response.headers.set('Access-Control-Allow-Origin', '*'); // Allow all origins or specify the allowed origin
-        response.headers.set('Access-Control-Allow-Methods', 'GET, POST, OPTIONS');
-        response.headers.set('Access-Control-Allow-Headers', 'X-Requested-With, Content-Type');
-
         return response;
     }
 }
