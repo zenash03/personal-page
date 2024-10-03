f464e869a63963a05959ad3940e9376644d3d4b1
wyrtzen <wyrtzen.ecc@gmail.com>
2024-10-04 01:04:09 +0700

    Fix hiding Address UI

--- components/SmoothScrolling.jsx
+++ components/SmoothScrolling.jsx
@@ -2,50 +2,31 @@
 import { ReactLenis } from "@studio-freight/react-lenis";
 import { useEffect, useRef } from "react";
 import gsap from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger"; // Import ScrollTrigger
+
+gsap.registerPlugin(ScrollTrigger); // Register ScrollTrigger
 
 function SmoothScrolling({ children }) {
   const lenisRef = useRef();
 
+  ScrollTrigger.normalizeScroll(true)
+  ScrollTrigger.config({ ignoreMobileResize: true})
+
   // GSAP and Lenis integration for scrolling
   useEffect(() => {
     function update(time) {
-      lenisRef.current?.lenis?.raf(time * 1000);
+      if (lenisRef.current?.lenis) {
+        lenisRef.current.lenis.raf(time * 1000);
+      }
     }
 
-    gsap.ticker.add(update);
-
-    return () => {
-      gsap.ticker.remove(update);
-    };
-  }, []);
-
-  // Throttled function for updating vh on resize
-  useEffect(() => {
-    const setVh = () => {
-      const vh = window.innerHeight * 0.01;
-      document.documentElement.style.setProperty("--vh", `${vh}px`);
-    };
-
-    // Throttle to limit how often resize is called
-    const throttleResize = () => {
-      clearTimeout(window.resizeThrottle);
-      window.resizeThrottle = setTimeout(setVh, 100);
-    };
-
-    // Initial setting of the vh
-    setVh();
-
-    // Update vh on resize
-    window.addEventListener("resize", throttleResize);
-
-    // Cleanup event listener on unmount
-    return () => window.removeEventListener("resize", throttleResize);
+    gsap.ticker.add(update); // Add GSAP ticker to update Lenis scrolling
   }, []);
 
   return (
     <ReactLenis
       ref={lenisRef}
-      autoRaf={false}
+      autoRaf={false} // Set to false since we are using GSAP's RAF
       root
       options={{ duration: 1, smoothTouch: true, touchMultiplier: 2 }}
     >
